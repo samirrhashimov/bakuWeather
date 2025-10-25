ab513f7a0b853cd47775f3e62ef7f643615d24ea
Samir Hashimov <samirrhashimov>
2025-10-25 13:47:58 +0400

    build tests

--- src/components/AzerbaijanMap.jsx
+++ src/components/AzerbaijanMap.jsx
@@ -1,11 +1,33 @@
 import { useEffect } from "react";
-import React from 'react'
+import React from "react";
 
 export default function CountryMap() {
   useEffect(() => {
-    if (window.simplemaps_countrymap) {
-      window.simplemaps_countrymap.load();
-    }
+
+    const dataScript = document.createElement("script");
+    dataScript.src = "../public/scripts/mapdata.js";
+    dataScript.async = true;
+
+    const countryScript = document.createElement("script");
+    countryScript.src = "../public/scripts/countrymap.js";
+    countryScript.async = true;
+
+    countryScript.onload = () => {
+      document.body.appendChild(dataScript);
+    };
+
+    dataScript.onload = () => {
+      if (window.simplemaps_countrymap) {
+        window.simplemaps_countrymap.load();
+      }
+    };
+
+    document.body.appendChild(countryScript);
+
+    return () => {
+      document.body.removeChild(countryScript);
+      document.body.removeChild(dataScript);
+    };
   }, []);
 
   return <div id="map" style={{ width: "100%", height: "500px" }}></div>;
